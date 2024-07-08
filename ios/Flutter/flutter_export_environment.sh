0c98d5cedb51f31553dee0f4dd8e886c318cd6ac
TheFinalGuardian <karthikeyaat@gmail.com>
2024-07-07 19:33:30 -0700

    yogesh needs home_page for development of bottom navigation bar

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\mruna\OneDrive - Bellevue School District\Documents\flutter"
+export "FLUTTER_ROOT=C:\Users\mruna\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\mruna\ammasevasadanam_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ammasevasadanam_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        )
-      )
+      home: HomePage()
     );
   }
 }
