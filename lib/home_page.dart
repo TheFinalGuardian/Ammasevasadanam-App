6af4555056133632533e3d700f93cbf5fc6f6b82
TheFinalGuardian <karthikeyaat@gmail.com>
2024-07-17 17:11:40 -0700

    some random stuff i figured i needed to commit bc they seemed to have changed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:ammasevasadanam_app/menu_bottom.dart';
 import 'package:flutter/material.dart';
 import 'package:ammasevasadanam_app/animations/swipe_animations/arrow_animation.dart';
@@ -12,38 +11,51 @@ class HomePage extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Column(
-              children: [
-                Text("Welcome To"),
-                Text("Ammasevasadanam"),
-              ],
-            ),
+      appBar: genAppBar(),
+      body: Column(
+        children: [
+          genInstructions(),
+          genArrowAnims(),
+        ],
+      ),
+      bottomNavigationBar: const MenuBottom(),
+    );
+  }
+
+  AppBar genAppBar() {
+    return AppBar(
+      title: const Center(
+        child: Text('Ammasevasadanam'),
+      ),
+    );
+  }
+
+  Widget genInstructions() {
+    return const Column(children: [
+      Text(
+          """    This application is meant to be used for logging expenses and donations. Admins will be able to see statistics using a password. """),
+      Text("To navigate between pages:"),
+    ]);
+  }
+
+  Widget genArrowAnims() {
+    return FittedBox(
+      child: Row(
+        children: [
+          const Column(
+            children: [
+              Text("SWIPE LEFT"),
+              ArrowAnimation(),
+            ],
           ),
-        ),
-        body: const Column(
-          children: [
-            Text(
-                """    This application is meant to be used for logging expenses and donations. Admins will be able to see statistics using a password. """),
-            Text("To navigate between pages:"),
-            FittedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text("SWIPE RIGHT"),
-                      ArrowAnimation(),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: const MenuBottom(),
-        );
+          Column(
+            children: [
+              const Text("SWIPE RIGHT"),
+              Transform.scale(scaleX: -1, child: const ArrowAnimation()),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
