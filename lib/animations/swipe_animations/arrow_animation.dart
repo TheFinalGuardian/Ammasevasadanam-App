e18acc68607116c4500463765232a517773e9623
YogeshwaraSai <yogesh.kurapaty@gmail.com>
2024-07-18 07:29:29 -0700

    made the animation twice as fast

--- lib/animations/swipe_animations/arrow_animation.dart
+++ lib/animations/swipe_animations/arrow_animation.dart
@@ -1,15 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math';
 
-Widget arrowAnim() {
-  return Row(
-    children: List<Widget>.generate(
-      4,
-      (int i) => ColorFiltered(
-          colorFilter: const ColorFilter.mode(Colors.black, BlendMode.modulate),
-          child: SvgPicture.asset('images/arrow.svg')),
-      growable: true,
-    ).animate(interval: 400.ms).fade().reversed.toList(),
-  );
+class ArrowAnimation extends StatefulWidget {
+  const ArrowAnimation({super.key});
+
+  @override
+  State<ArrowAnimation> createState() => _ArrowAnimationState();
+}
+
+class _ArrowAnimationState extends State<ArrowAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: List<Widget>.generate(
+        4,
+        (int i) => ColorFiltered(
+            colorFilter:
+                const ColorFilter.mode(Colors.black, BlendMode.modulate),
+            child: SvgPicture.asset('images/arrow.svg')),
+        growable: true,
+      )
+          .animate(interval: 1.seconds)
+          .fade(
+              duration: 80.seconds,
+              curve: const SineCurve(count: 10)
+            )
+          .reversed
+          .toList(),
+    );
+  }
+}
+
+class SineCurve extends Curve {
+  final double count;
+
+  const SineCurve({this.count = 3});
+
+  // t = x
+  @override
+  double transformInternal(double t) {
+    var val = sin(count * 2 * pi * t) + 0.5;
+    return val; //f(x)
+  }
 }
