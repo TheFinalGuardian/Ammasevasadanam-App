f8f40e0bcaec319cadab916d663e91ce0bd6698d
TheFinalGuardian <karthikeyaat@gmail.com>
2024-07-22 17:34:16 -0700

    updating yogesh's windows legacy imports

--- lib/animations/swipe_animations/swipe_pages/swipe_log_page.dart
+++ lib/animations/swipe_animations/swipe_pages/swipe_log_page.dart
@@ -1,7 +1,6 @@
+import 'package:ammasevasadanam_app/animations/swipe_animations/swipe_pages/swipe_home_page.dart';
 import 'package:ammasevasadanam_app/animations/transition_animation_folder/right_to_left.dart';
-import 'package:ammasevasadanam_app/home_page.dart';
-import 'package:ammasevasadanam_app/pages_with_swipe_animation/home_page_with_animation.dart';
-import 'package:ammasevasadanam_app/place_holders/log_page_place_holder_folder/log_page_place_holder.dart';
+import 'package:ammasevasadanam_app/log_page_stuff/log_page.dart';
 import 'package:flutter/material.dart';
 
 class LogPageWithAnimation extends StatelessWidget {
