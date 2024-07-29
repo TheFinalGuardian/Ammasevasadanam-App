52cf99385f0953d95a3147ff93d28b22cf17263f
TheFinalGuardian <karthikeyaat@gmail.com>
2024-07-29 14:17:08 -0700

    commit for pull

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,36 +7,42 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Column(
-              children: [
-                Text("Welcome To"),
-                Text("Ammasevasadanam"),
-              ],
-            ),
+      appBar: AppBar(
+        title: const Center(
+          child: Column(
+            children: [
+              Text("Welcome To"),
+              Text("Ammasevasadanam"),
+            ],
           ),
         ),
-        body: const Column(
-          children: [
-            Text(
-                """    This application is meant to be used for logging expenses and donations. Admins will be able to see statistics using a password. """),
-            Text("To navigate between pages:"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("SWIPE RIGHT"),
-
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-        bottomNavigationBar: const MenuBottom(),
-        );
+      ),
+      body: const Column(
+        children: [
+          Text(
+              """    This application is meant to be used for logging expenses and donations. Admins will be able to see statistics using a password. """),
+          Text("To navigate between pages:"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("SWIPE LEFT"),
+                ],
+              ),
+              Text("or"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text("SWIPE RIGHT"),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: const MenuBottom(),
+    );
   }
 }
